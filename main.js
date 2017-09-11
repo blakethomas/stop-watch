943eda2cd5606016d8f969f2c3ea172dc313cb1d
blakethomas <blakekthomas49@gmail.com>
2017-09-11 14:44:46 -0700

    adds stylesheet and toggles start button

--- main.js
+++ main.js
@@ -1,47 +1,44 @@
 var time = 0
-var limit = null
 var id = null
 var $time = document.querySelector('#time')
 var $start = document.querySelector('#start')
 var $stop = document.querySelector('#stop')
 
-function update(){
+function update() {
   return time = time + 1
 }
 
-function updateDom(){
+function updateDom() {
   time.toString()
   return $time.textContent = time
 }
 
-function removeStart(){
-  if (id !== null) {
-    $start.remove()
-  }
+function toggleStart() {
+  $start.classList.toggle('startbutton')
   return
 }
 
-function startTimer(){
-  if(id === null){
+function startTimer() {
+  if (id === null) {
     id = window.setInterval(update, 1000)
     window.setInterval(updateDom, 16)
   }
 }
 
-
-function stopTimer(){
+function stopTimer() {
   window.clearInterval(id)
   id = null
   return
 }
 
-$start.addEventListener('click', function(event){
+$start.addEventListener('click', function(event) {
   event.preventDefault()
   startTimer()
-  removeStart()
-});
+  toggleStart()
+})
 
-$stop.addEventListener('click', function(event){
+$stop.addEventListener('click', function(event) {
   event.preventDefault()
   stopTimer()
+  toggleStart()
 })
