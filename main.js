d02a4fcf77f2d71836bab8a69092fc0b0c6cd7a1
blakethomas <blakekthomas49@gmail.com>
2017-09-11 09:42:58 -0700

    adds an event listener which changes the time variable every second

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ var time = 0;
 var $time = document.querySelector('#time')
 var $start = document.querySelector('#start')
 var $stop = document.querySelector('#stop')
+
+function update(){
+  return time = time + 1
+}
+
+
+$start.addEventListener('click', function(event){
+  event.preventDefault()
+  window.setInterval(update, 1000)
+});
